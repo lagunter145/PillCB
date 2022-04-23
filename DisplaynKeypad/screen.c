281c0632bf81959c2da9bc9b6d7473ca2f7ed6de
lizziesarah <102872411+lizziesarah@users.noreply.github.com>
2022-04-23 16:54:05 -0400

    Update screen.c

--- DisplaynKeypad/screen.c
+++ DisplaynKeypad/screen.c
@@ -21,6 +21,178 @@ int pill1_time;
 int pill2_time;
 int pill3_time;
 
+// clock vars
+int seconds = 0;
+int minutes = 0;
+int hours = 0;
+int fastkeyhit = 0;
+extern int county1;
+//int county2 = 0;
+//int county3 = 0;
+extern int hr1;
+extern int min1;
+extern int sec1;
+/*
+int input_math(int county, int hrs, int mins, int secs) {
+    if(county != 0) {
+
+        hrs = county / 10000;
+
+        mins = (county / 100) % 100;
+
+        secs = county % 100;
+    }
+
+    char s11[20];
+    char s12[20];
+    char s13[20];
+    sprintf(s11, "%d", hrs);
+    sprintf(s12, "%d", mins);
+    sprintf(s13, "%d", secs);
+    strcat(s11, s12);
+    strcat(s11, s13);
+    int c1 = atoi(s11);
+    return c1;
+}
+*/
+int timer_pill1(int counter){
+
+    //calculate display values for Pill 1
+
+        if (counter == 60){
+            sec1--;
+        }
+         if (sec1 == -1) {
+             min1--;
+             sec1 = 59;
+         }
+         if (min1 == -1) {
+             hr1--;
+             min1 = 59;
+
+         }
+/*
+         if (hr1 == 0 && min1 == 0 && sec1 == 0) {
+
+           //TRIGGER MESSAGE FUNCTION
+
+             init_wavetable_hybrid2();
+             init_dac();
+             init_tim6();
+             MIDI_Player *mp = midi_init(midifile);
+             // The default rate for a MIDI file is 2 beats per second
+             // with 48 ticks per beat.  That's 500000/48 microseconds.
+             init_tim2(10417);
+             for(;;) {
+                 asm("wfi");
+                 // If we hit the end of the MIDI file, start over.
+                 if (mp->nexttick == MAXTICKS)
+                     mp = midi_init(midifile);
+
+
+         }
+*/
+
+         char s11[20];
+         char s12[20];
+         char s13[20];
+         sprintf(s11, "%d", hr1);
+         sprintf(s12, "%d", min1);
+         sprintf(s13, "%d", sec1);
+         strcat(s11, s12);
+         strcat(s11, s13);
+         int c1 = atoi(s11);
+         return c1;
+
+}
+
+int calc_clock(int counter) {
+    //normal mode
+    if(fastkeyhit == 0) {
+        if (counter == 60){
+            seconds++;
+            //counter = 0;
+        }
+
+        if (seconds == 60){
+            minutes++;
+            seconds = 0;
+        }
+
+        if (minutes == 60){
+            hours++;
+            minutes = 0;
+        }
+
+        if (hours == 24) {
+            hours = 0;
+        }
+    }
+
+    //only do this if fast mode key is hit, how do we do this?
+        //demo mode
+        if (fastkeyhit != 0) { //I guess this is one way?
+            if (counter == 1){
+                seconds++;
+                counter = 0;
+            }
+            if (seconds == 60){
+                minutes++;
+                seconds = 0;
+            }
+            if (minutes == 60){
+                hours++;
+                minutes = 0;
+            }
+            if (hours == 24) {
+                hours = 0;
+            }
+        }
+            char s1[20];
+            char s2[20];
+            char s3[20];
+            sprintf(s1, "%d", hours);
+            sprintf(s2, "%d", minutes);
+            sprintf(s3, "%d", seconds);
+            strcat(s1, s2);
+            strcat(s1, s3);
+            int c = atoi(s1);
+            return c;
+}
+
+void print_clock(int time) {
+    // ur mom ....... jk its print_clock@!
+    LCD_Clear(BLACK);
+    int hrs1 = time / 100000;
+    int hrs2 = time / 10000 % 10;
+    int mins1 = time / 1000 % 10;
+    int mins2 = time / 100 % 10;
+    int secs1 = time / 10 % 10;
+    int secs2 = time % 10;
+
+    char s1[10];
+    char s2[10];
+    char s3[10];
+    char s4[10];
+    char s5[10];
+    char s6[10];
+
+    sprintf(s1, "%d", hrs1);
+    sprintf(s2, "%d:", hrs2);
+    sprintf(s3, "%d", mins1);
+    sprintf(s4, "%d:", mins2);
+    sprintf(s5, "%d", secs1);
+    sprintf(s6, "%d", secs2);
+
+    strcat(s1, s2);
+    strcat(s1, s3);
+    strcat(s1, s4);
+    strcat(s1, s5);
+    strcat(s1, s6);
+
+    LCD_DrawString(90,100, WHITE, WHITE, s1, 16, 1);
+}
+
 void home_screen(void) {
     // default screen
     LCD_Clear(BLACK);
@@ -234,6 +406,7 @@ void add_edit_pill(struct Pill p) {
         strcat(s1, s6);
         LCD_DrawFillRectangle(80, 100, 180, 120, BLACK);
         LCD_DrawString(90,100, WHITE, WHITE, s1, 16, 1);
+        key_press = get_keypress();
     }
     if(key_press == '*') {
         pill_menu();
@@ -252,4 +425,6 @@ void set_time(void) {
     LCD_DrawString(20,220, WHITE, WHITE, "B - Edit Previous Digit", 16, 1);
     LCD_DrawString(20,250, WHITE, WHITE, "* - Back to General Menu", 16, 1);
 }
+
+
 
